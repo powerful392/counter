72520e39dbc79711541d51e60f2c2af4f12bf82c
powerful392 <fatemehzahra810@gmail.com>
2025-12-16 19:13:36 +0330

    Fixing the bug related to the selection of languages, and making the code more compact رفع باگ مربوط به انتخاب زبانها، و خانا تر کردن کد

--- counter.py
+++ counter.py
@@ -1,7 +1,6 @@
-language = input("please select a language between persian and english, type pe, or persian for persian, type en, or english for english")
-while language.lower().strip() != "en" and language.lower().strip() != "english" and language.lower().strip() != "pe" and language.lower().strip() != "persian":
-    language = input("please just choose english or persian languages, and don't type other things, in this edit box")
-    if language.lower().strip() == "en" or language.lower().strip() == "english":
+while True:
+    language = input("please select a language between persian and english, type pe, or persian for persian, type en, or english for english")
+    if language.lower().strip() in ["en", "english"]:
         question = input("do you want to see when number will be count? type yes or no")
         counter = 0
         user_counts = input("if you press enter, number will be count, and for end, type end to end counting")
@@ -15,7 +14,7 @@ while language.lower().strip() != "en" and language.lower().strip() != "english"
                 counter += 1
                 if question.lower().strip() == "yes":
                     print(f"Current count: {counter}")
-            if user_counts.strip() == "end":
+            elif user_counts.strip() == "end":
                 print(f"Final result is {counter}")
                 break
         finish = input("Counter is finished, type 'exit' to close: ")
@@ -23,7 +22,7 @@ while language.lower().strip() != "en" and language.lower().strip() != "english"
             finish = input("Please type 'exit' to close: ")
         print("Program closed!")
         break
-    elif language.lower().strip() == "pe" or language.lower().strip() == "persian":
+    elif language.lower().strip()in ["pe", "persian"]:
         question = input("آیا دوست دارید شمارش اعداد رو در همون لحظه ببینید؟ بله یا خیر رو توی این ادیت باکس تایپ کنید")
         counter = 0
         user_counts = input("اگر اینتر بزنید، عدد شمارش میشه، و برای پایان شمارش، پایان رو بنویسید")
@@ -37,7 +36,7 @@ while language.lower().strip() != "en" and language.lower().strip() != "english"
                 counter += 1
                 if question.strip() == "بله":
                     print(f"شمارش کنونی، {counter}")
-            if user_counts.strip() == "پایان":
+            elif user_counts.strip() == "پایان":
                 print(f"نتیجه ی شمارش نهایی {counter} هست")
                 break
         finish = input("شمارنده تمام شد، خروج رو برای بستن بنویسید")
@@ -45,3 +44,6 @@ while language.lower().strip() != "en" and language.lower().strip() != "english"
             finish = input("لطفاً خروج رو برای بسته شدن بنویسید")
         print("برنامه بسته شد!")
         break
+    else:
+        print("please just choose english or persian languages, and don't type other things, in this edit box")
+    print("لطفاً فقط زبان فارسی یا انگلیسی رو انتخاب کنید و چیز دیگه ای ننویسید")
